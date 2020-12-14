ef8fc8b32edbfebcf3ca977368cc959250b42c74
MolchanovaDG <pantileeva95@mail.ru>
2020-12-14 21:59:28 +0700

    project

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -1,10 +1,11 @@
-import React, {useReducer} from "react";
-import {AlertContext} from "./alertContext";
+import React, {useReducer} from 'react'
+import {AlertContext} from './alertContext'
 import {alertReducer} from './alertReducer'
-import {HIDE_ALERT, SHOW_ALERT} from "../types";
+import {HIDE_ALERT, SHOW_ALERT} from '../types'
 
 export const AlertState = ({children}) => {
     const [state, dispatch] = useReducer(alertReducer, {visible: false})
+
     const show = (text, type = 'warning') => {
         dispatch({
             type: SHOW_ALERT,
@@ -13,7 +14,8 @@ export const AlertState = ({children}) => {
     }
 
     const hide = () => dispatch({type: HIDE_ALERT})
-    return(
+
+    return (
         <AlertContext.Provider value={{
             show, hide,
             alert: state
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,27 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useContext, useEffect} from 'react'
 import {Form} from '../components/Form'
-import {Notes} from "../components/Notes";
+import {Notes} from '../components/Notes'
+import {FirebaseContext} from '../context/firebase/firebaseContext'
+import {Loader} from '../components/Loader'
 
-export const Home = () =>{
-    const notes = [...Array(3)
-        .keys()]
-        .map(item => ({id: item, title: `Note ${item}`}))
-    return(
+export const Home = () => {
+    const {loading, notes, fetchNotes, removeNote} = useContext(FirebaseContext)
+
+    useEffect(() => {
+        fetchNotes()
+        // eslint-disable-next-line
+    }, [])
+
+    return (
         <Fragment>
-            <Form/>
-            <hr/>
-            <Notes notes={notes}/>
+            <Form />
 
+            <hr/>
+
+            {loading
+                ? <Loader />
+                : <Notes notes={notes} onRemove={removeNote} />
+            }
         </Fragment>
     )
 }
